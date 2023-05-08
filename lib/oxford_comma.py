33e2c4317d86cb3c428264e43c4293468be932bc
+ <+>
2023-05-08 00:20:12 -0400

    satisfy deliverables

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,6 @@
 def oxford_comma(items):
-    return None
+    while items:
+        if len(items) == 2:
+            return " and ".join(items)
+        else:
+            return ", ".join(items[:-2] + [", and ".join(items[-2:])])
